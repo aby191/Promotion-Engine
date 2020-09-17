156fd416946ae7f4bdfa879da75242a354920634
Abhinav Upadhyay <upadhyay.abhinav.cs13@outlook.com>
2020-09-17 23:55:39 +0530

    Pushing the structure as per the understanding initially.

---
+++ Promotion Engine/Promotion Engine_UnitTest/PromotionEngine.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Promotion_Engine_UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
