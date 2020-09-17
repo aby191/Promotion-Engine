bf775b5f0728e61d0013c61061fb0111e4f2a97b
Abhinav Upadhyay <upadhyay.abhinav.cs13@outlook.com>
2020-09-18 01:23:22 +0530

    Committing the initial development changes

--- Promotion Engine/Promotion Engine/Program.cs
+++ Promotion Engine/Promotion Engine/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Promotion_Engine
 {
@@ -6,7 +8,103 @@ namespace Promotion_Engine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (int counter = 0; ; counter++)
+            {
+                List<Product> products = new List<Product>();
+
+                Console.WriteLine("\n" + "Please enter the total number of order");
+                int totalorders = Convert.ToInt32(Console.ReadLine());
+                for (int numofproduct = 0; numofproduct < totalorders; numofproduct++)
+                {
+                    Console.WriteLine("Please enter product from the list:A,B,C or D");
+                    string productType = Console.ReadLine();
+                    Product product = new Product(productType);
+                    products.Add(product);
+                }
+
+                int totalCost = GetTotalCostofProducts(products);
+                Console.WriteLine("The Total Price of order is " + totalCost);
+            }
+            Console.ReadLine();
+        }
+
+        public static int GetTotalCostofProducts(List<Product> products)
+        {
+            int counterofA = 0;
+            int costOfA = 50;
+            int counterofB = 0;
+            int costOfB = 30;
+            int CounterofC = 0;
+            int costOfC = 20;
+            int CounterofD = 0;
+            int costOfD = 15;
+            foreach (Product pr in products)
+            {
+                if (pr.Id == "A" || pr.Id == "a")
+                {
+                    counterofA = counterofA + 1;
+                }
+                if (pr.Id == "B" || pr.Id == "b")
+                {
+                    counterofB = counterofB + 1;
+                }
+                if (pr.Id == "C" || pr.Id == "c")
+                {
+                    CounterofC = CounterofC + 1;
+                }
+                if (pr.Id == "D" || pr.Id == "d")
+                {
+                    CounterofD = CounterofD + 1;
+                }
+            }
+
+
+            // This part can be handled by Dependency Injection
+            AProductPack Apack = new AProductPack();
+            BProductPack Bpack = new BProductPack();
+            CProductPack Cpack = new CProductPack();
+            DProductPack Dpack = new DProductPack();
+
+            int totalCostOfA = Apack.GetPrice(counterofA, costOfA);
+            int totalCostOfB = Bpack.GetPrice(counterofB, costOfB);
+            int totalCostOfC = Cpack.GetPrice(CounterofC, costOfC);
+            int totalCostofD = Dpack.GetPrice(CounterofD, costOfD);
+            int Sum= totalCostOfA + totalCostOfB + totalCostOfC + totalCostofD;
+            return Sum;
+
+        }
+    }
+
+
+    interface IPack { int GetPrice(int count, int costOfA); }
+    
+    class AProductPack : IPack
+    {
+        public int ProductsCount { get; set; }
+        public int GetPrice(int counterofA, int costOfA)
+        {
+            return (counterofA / 3) * 130 + (counterofA % 3 * costOfA);
+        }
+    }
+    class BProductPack : IPack
+    {
+        public int ProductsCount { get; set; }
+        public int GetPrice(int counterofB, int costOfB)
+        {
+            return (counterofB / 2) * 45 + (counterofB % 2 * costOfB);
+        }
+    }
+    class CProductPack : IPack
+    {
+        public int ProductsCount { get; set; }
+        public int GetPrice(int counterofC, int costOfC) => (counterofC * costOfC);
+    }
+    class DProductPack : IPack
+    {
+        public int ProductsCount { get; set; }
+        public int GetPrice(int counterofD, int costOfD)
+        {
+            return (counterofD * costOfD);
         }
     }
 }
